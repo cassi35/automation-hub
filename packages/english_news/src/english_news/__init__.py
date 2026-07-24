37a8ec161ab5ca02ef8b70b0b9daa96ac2ea438e
cassi35 <cgrsobral@gmail.com>
2026-07-24 14:00:54 -0300

    fix: fixed the import

--- packages/english_news/src/english_news/__init__.py
+++ packages/english_news/src/english_news/__init__.py
@@ -1,10 +1,5 @@
-from packages.shared.config.config import Config
-
-
 def main() -> None:
     print("Hello from english-news!")
-    print(Config.JIRA_TKOEN)
-
 
 if __name__ == "__main__":
     main()
--- packages/shared/registry/manifest.py
+++
@@ -1,7 +0,0 @@
-class AutomationManifest:
-    def __init__(self, slug, name, description, trigger_type, schedule=None):
-        self.slug = slug
-        self.name = name
-        self.description = description
-        self.trigger_type = trigger_type
-        self.schedule = schedule
