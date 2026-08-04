ad93ce3fe651e442026b73b14e962a87f7cfb4d3
cassi35 <cgrsobral@gmail.com>
2026-08-04 20:08:48 -0300

    .

--- apps/automations-hub/src/automations_hub/infra/db.py
+++ apps/automations-hub/src/automations_hub/infra/db.py
@@ -1,6 +1,6 @@
 from shared.db.settings.connection import BDConnectionHandler
-database = BDConnectionHandler()
 from rich import print
+database = BDConnectionHandler()
 async def init_db()->BDConnectionHandler:
     print('[green]Initializing database...[/green]')
     with database as db:
